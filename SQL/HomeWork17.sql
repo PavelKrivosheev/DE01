80ae3c6fdd28bf7a3d82d988de91d8a83abf88d6
PavelKrivosheev <answer3smart@gmail.com>
2024-11-18 22:24:27 +0300

    Add files via upload

---
+++ SQL/HomeWork17.sql
@@ -0,0 +1,16 @@
+-- Создаем таблицs.
+
+/*CREATE TABLE StudentTable (Student_Id INT
+					 , [Name] VARCHAR(30)
+					 , Course_Id INT)
+
+
+
+CREATE TABLE CourseTable (Course_Id INT
+					 , Course_Name VARCHAR(30)
+					 , Teacher VARCHAR(30))
+*/
+	
+	
+
+
